f255c4a15e606fcb674732ce7de1c175cc0f5afb
evangeline <ebubemmayie@gmail.com>
2024-11-27 00:30:19 +0100

    fix: fixed the blank index screen issue

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,8 @@ import { Redirect } from "expo-router";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { auth } from "@/firebaseConfig";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { images} from "@/constants";
 
 export default function Home() {
   const [user, setUser] = useState<User | null>(null);
@@ -18,7 +19,11 @@ export default function Home() {
   }, []);
 
   if (loading) {
-    return null
+    return (
+        <View className="flex justify-center items-center">
+            <Image source={images.iconLogo} resizeMode="center" />
+        </View>
+    )
   }
 
   if (user) {
--- constants/images.js
+++ constants/images.js
@@ -1,9 +1,11 @@
 import profile from "../assets/images/kemal.jpg";
 import job from "../assets/images/job.png"
 import logo from "../assets/logo.png"
+import iconLogo from "@/assets/icon.png"
 
 export default {
   profile,
   job,
-  logo
+  logo,
+  iconLogo
 };
